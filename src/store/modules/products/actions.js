085e2aba79a58806546b63a0d87d33ccc62fb66d
wesdeveloper <wesleyalopes2010@gmail.com>
2020-06-06 11:07:51 -0300

    fix: 🐛 remove not used action

--- src/store/modules/products/actions.js
+++ src/store/modules/products/actions.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 import { API_URL } from '../../../config/constants';
 
-export const GET_PRODUCTS = '@PRODUCTS/GET_PRODUCTS';
 export const GET_PRODUCTS_REQUEST = '@PRODUCT/GET_PRODUCTS_REQUEST';
 export const GET_PRODUCTS_SUCCESS = '@PRODUCT/GET_PRODUCTS_SUCCESS';
 export const GET_PRODUCTS_ERROR = '@PRODUCT/GET_PRODUCTS_ERROR';
-
-export const getProducts = () => ({ type: GET_PRODUCTS });
 
 export const getProductsRequest = () => ({ type: GET_PRODUCTS_REQUEST });
 
